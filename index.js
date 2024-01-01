60c4c10bd3b4c0c489d7c4509d7519a9af7c4d91
Krishna <106014328+krishnask18@users.noreply.github.com>
2024-01-01 16:53:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var express = require('express')
-const bodyParser = require('body-parser');
 const cors = require('cors')
 app = express()
 port = 5000
@@ -8,32 +7,44 @@ function listener(){
     console.log("Listening at port "+port)
 }
 app.use(cors())
-var msgs = ""
-
 var datamap = new Map();
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.json());
+var msgs = ""
 function home(req, res){
     msgs = ""
     console.log("Request Recieved \n"+"msg => "+req.query.nme+":"+req.query.msg)
-    msgs+=(req.query.nme+":"+req.query.msg+"<br>");
-    [...datamap.keys()].forEach((key) => {
-    datamap.set(key, datamap.get(key)+msgs);
+    msgs+=(req.query.nme+" : "+req.query.msg+"<br>");
+    var tempmap = datamap.get(req.query.code);
+    [...tempmap.keys()].forEach((key) => {
+    tempmap.set(key, tempmap.get(key)+msgs);
     });
+    datamap.set(req.query.code, tempmap)
     console.log(datamap)
     res.send("Krishna")
 }
 
 function senddata(req, res){
-    res.send(datamap.get(req.query.nme))
-    datamap.set(req.query.nme, "")
+    if(datamap.get(req.query.code) == undefined){
+        datamap.set(req.query.code, new Map())
+    }
+    var tempmap = datamap.get(req.query.code)
+    if(tempmap.get(req.query.nme) == undefined){
+        tempmap.set(req.query.nme, "")
+    }
+    res.send(tempmap.get(req.query.nme))
+    tempmap.set(req.query.nme, "")
+    datamap.set(req.query.code, tempmap)
 }
 
 function delName(req, res){
     try{
-        datamap.delete(req.query.nme);
+        var tempmap = datamap.get(req.query.code)
+        tempmap.delete(req.query.nme)
     }
     catch(e){}
+    if([...tempmap.keys()].length == 0){
+        datamap.delete(req.query.code)
+    }
+    else datamap.set(req.query.code, tempmap)
     console.log(datamap)
 }
 app.delete('/', delName)
